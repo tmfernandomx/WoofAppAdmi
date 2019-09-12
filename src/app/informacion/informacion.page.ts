c359ace04b1dd302c924ff365255142aec83c478
Antonio <anhr9728@gmail.com>
2019-09-12 00:49:39 -0500

    Implementacion del servicio de auth y registro de los campos en firebase cambio en el boton de registro se puso antes de las redes sociales -Se consultan ya los datos de los usuarios para mostrarlos en el apartado del perfil - -Corregir la navegacion de las ventanas

--- src/app/informacion/informacion.page.ts
+++ src/app/informacion/informacion.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './../servicios/auth.service';
+import { User } from '../models/users.interface';
+import { UsersService } from '../servicios/users.service';
+import { LoadingController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-informacion',
@@ -8,15 +11,58 @@ import { AuthService } from './../servicios/auth.service';
 })
 export class InformacionPage implements OnInit {
 
-  constructor(public authService :AuthService) { }
+  user: User = {
+    nombre: '',
+    apellidos: '',
+    domicilio: '',
+    telefono: '',
+    foto: ''
+  };
+
+  userId = null;
+
+  constructor(
+    private authService: AuthService,
+    private nav: NavController,
+    private _user: UsersService,
+    private loadingController: LoadingController) {
+      this.userId = localStorage.getItem('id');
+    }
 
   ngOnInit() {
+    this.loadData();
   }
 
+  async loadData() {
+    const loading = await this.loadingController.create({
+      message: 'Cargando...'
+    });
+    await loading.present();
+    this._user.getUserById(this.userId).subscribe( data => {
+      console.log(data);
+      this.user = data;
+      loading.dismiss();
+    });
+  }
 
-  Onlogout(){
+  async saveDataUser() {
+    const loading = await this.loadingController.create({
+      message: 'Guardando....'
+    });
 
-this.authService.logout();
+    await loading.present();
+
+    if(this.userId != null) {
+      //uppdate
+      this._user.updateUser(this.user, this.userId).then(() => {
+          loading.dismiss();
+          this.nav.navigateForward('/selugar');
+      });
+    }
+  }
+
+  Onlogout() {
+    this.authService.logout();
   }
 
 }
--- src/app/loginn/loginn.page.ts
+++ src/app/loginn/loginn.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService } from '../servicios/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-loginn',
   templateUrl: './loginn.page.html',
@@ -10,16 +11,17 @@ export class LoginnPage implements OnInit {
   public email: string;
 public password: string;
 
-  constructor(private auth : AuthService) { }
+  constructor(private auth: AuthService,
+              private router: Router) { }
 
   ngOnInit() {
   }
 
 onSubmitRegister(){
 this.auth.register(this.email, this.password).then(auth => {
-  console.log(auth)
-
-}).catch(err => console.log(err))
+  console.log(auth);
+  this.router.navigate(['tabs/informacion']);
+}).catch(err => console.log(err));
 }
 
 }
